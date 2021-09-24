2d6e92342bf99c7fc08548f4a67e75e7af9efbd9
amitnahtomi <amit.nahtomi@gmail.com>
2021-09-24 11:43:38 +0300

    handle localstorage

--- solution/index.js
+++ solution/index.js
@@ -3,36 +3,95 @@ function handleAddTask (event){
     if(newTask === "")
     alert("can't add empty task");
     let newTaskEl = document.createElement("li");
-    newTaskEl.innerHTML = newTask;
+    newTaskEl.classList.add("task");
+    newTaskEl.innerText = newTask;
     let taskList = document.getElementsByTagName("ul")[event.target.parentNode.id];
-    taskList.append(newTaskEl);
-    localStorage.getItem("todo").push(JSON.stringify(newTaskEl));
+    taskList.insertBefore(newTaskEl, taskList.childNodes[0]);
+    //const tasks = localStorage.getItem("tasks");
+    tasks[event.target.parentNode.dataset.action].unshift(newTask);  
+    localStorage.setItem("tasks", JSON.stringify(tasks));  
+    
 }
 function handleDBclick (event){
+    if(event.target.tagName !== "LI") return;
     let tempInput = document.createElement("input");
     tempInput.setAttribute("type", "text");
     tempInput.setAttribute("value", event.target.innerHTML);
+    let tempI = tasks[event.target.closest("section").dataset.action].indexOf(tempInput.value);
     event.target.innerHTML = "";
     event.target.append(tempInput);
-    event.target.onkeydown = (key) => {
-        if(key.keyCode === 13)
+    tempInput.onblur = () => {
+        tasks[event.target.closest("section").dataset.action][tempI] = tempInput.value;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
         event.target.innerHTML = tempInput.value;
     }
 }
+function loadTasks () {
+    if(!localStorage.getItem("tasks"))
+    localStorage.setItem("tasks", JSON.stringify(tasks)) //return false;
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    //tasks = localStorage.getitem("tasks");
+    //tasks = json.parse(tasks);
+    let classI = 0;
+    let newLi;
+    for(let task in tasks){
+        for(let i = 0; i < tasks[task].length; i++){
+            newLi =  document.createElement("li");
+            newLi.innerHTML = tasks[task][i];
+            document.getElementById(classI).getElementsByTagName("ul")[0].append(newLi);
+        }
+        classI++;
+    }
+    return true;
+}
 function handleMouseOver(event) {
-    if(event.altKey === true && event.keyCode === 50){
-       document.getElementsByClassName("in-progress-tasks")[0].append(event.target.parentNode);
+    if(event.target.tagName !== "LI") return;
+    //if(event.altKey === true){
+    //event.target.addEventListener("keypress", (key) =>{
+      //if(key.charCode === 2){ 
+         // alert("sdf"); 
+      // document.getElementsByClassName("in-progress-tasks")[0].append(event.target);
+    
+}
+function search (key) {
+    let liArr = document.querySelectorAll("li");
+    console.log(key.key);
+    let query;
+        query = document.getElementById("search").value;
+    for(let i = 0; i < liArr.length; i++){
+        if(liArr[i].innerText.includes(query) === false)
+        liArr[i].hidden = true;
+        else
+        liArr[i].hidden = false;
+        
     }
 }
-    //alert("svd");
-        /*if(event.getModifierState("alt") || key.keyCode === 49){
-        document.getElementsByClassName("to-do-tasks")[0].append(event.target);
-    }
-        if(event.getModifierState("alt") || key.keyCode === 50)
-        document.getElementsByClassName("in-progress-tasks")[0].append(event.target);
-    }*/
+let tasks = {
+    todo: [],
+    "in-progress": [],
+    done: []
+}
+//localStorage.setItem("tasks", JSON.stringify(itemsArray));
+//const tasks = JSON.parse(localStorage.getItem("tasks")); 
+//if(!loadTasks()){
+//localStorage.setItem("tasks", JSON.stringify(tasks));
+//}
+loadTasks();
 document.getElementById("submit-add-to-do").addEventListener("click", handleAddTask);
-document.getElementsByClassName("to-do-tasks")[0].addEventListener("dblclick", handleDBclick);
-document.getElementsByClassName("to-do-tasks")[0].addEventListener("keypress", handleMouseOver);
-let todoArr = ["dsadsa", "asds", "asdsd"];
-localStorage.setItem("todo",JSON.stringify(todoArr));
+document.getElementById("submit-add-in-progress").addEventListener("click", handleAddTask);
+document.getElementById("submit-add-done").addEventListener("click", handleAddTask);
+document.addEventListener("dblclick", handleDBclick);
+document.addEventListener("mouseover", handleMouseOver);
+document.getElementById("search").addEventListener("keyup", search);
+
+/*let tasks = {
+    todo: [],
+    "in-progress": [],
+    done: []
+}
+if(!loadTasks()){
+localStorage.setItem("tasks", JSON.stringify(tasks));
+}*/
+
+
+
