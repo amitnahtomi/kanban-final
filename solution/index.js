c55f79d1298e01816400a347c0dc67ddba459775
amitnahtomi <amit.nahtomi@gmail.com>
2021-09-26 22:42:09 +0300

    adding comments

--- solution/index.js
+++ solution/index.js
@@ -1,4 +1,4 @@
-
+// Add new Task 
 function handleAddTask (event){
     let newTask = event.target.nextElementSibling.value;
     if(newTask === ""){
@@ -12,9 +12,10 @@ function handleAddTask (event){
     let taskList = document.getElementsByTagName("ul")[event.target.parentNode.id];
     taskList.insertBefore(newTaskEl, taskList.childNodes[0]);
     tasks[event.target.parentNode.dataset.action].unshift(newTask);  
-    localStorage.setItem("tasks", JSON.stringify(tasks));  
+    localStorage.setItem("tasks", JSON.stringify(tasks));  //save to local storage
     
 }
+//Edit Task
 function handleDBclick (event){
     if(event.target.tagName !== "LI") return;
     let task = event.target.innerText;
@@ -23,80 +24,86 @@ function handleDBclick (event){
     event.target.contentEditable = true;
     event.target.focus();
     event.target.onblur = () => {
-        if(event.target.innerText=="") {
+        if(event.target.innerText=="") { //prevent saving empty task
             event.target.focus();
             return;
         }
         let updateTask = event.target.innerText;
         tasks[targetArr][taskI] = updateTask;
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("tasks", JSON.stringify(tasks)); //save to local storage
         event.target.contentEditable = false
     }
     
 }
+//load tasks from local storage
 function loadTasks () {
-    if(!localStorage.getItem("tasks"))
+    if(!localStorage.getItem("tasks")) //if no tasks found in local storage save empty tasks jason
         localStorage.setItem("tasks", JSON.stringify(tasks))
     tasks = JSON.parse(localStorage.getItem("tasks"));
     let classI = 0;
     let newLi;
-    for(let task in tasks){
-        for(let i = 0; i < tasks[task].length; i++){
+    for(let taskArr in tasks){ //loop over task arrays (to-do \ in progress \ done)
+        for(let i = 0; i < tasks[taskArr].length; i++){ //loop over each array
             newLi =  document.createElement("li");
             newLi.classList.add("task");
-            newLi.innerHTML = tasks[task][i];
+            newLi.innerHTML = tasks[taskArr][i];
             document.getElementsByTagName("ul")[classI].append(newLi);
         }
         classI++;
     }
-    return true;
 }
+
+//handle task mouse over
 function handleMouseOver(event) {
     if(event.target.tagName !== "LI") return; 
     event.target.classList.add("active");
     document.addEventListener("keydown", keyPressMove);
 }
+
+//handles task move to another section, on mouse over + alt+1-3
 function keyPressMove(e) {
-    let task = document.getElementsByClassName("active")[0];
-    let prevUl = task.closest("section").dataset.action;
-    prevUlindex = tasks[prevUl].indexOf(task.innerText)
+    let task = document.getElementsByClassName("active")[0]; //get the active (hovered) task
+    let prevUl = task.closest("section").dataset.action; //get active task section
+    let prevTaskindex = tasks[prevUl].indexOf(task.innerText) //get active task index
     if(e.altKey && (e.key === "1" || e.key === "2" || e.key === "3")){
         let indexUl = e.key - 1;
-        let targetUl = document.getElementsByTagName("ul")[indexUl];
-        targetUl.insertBefore(task, targetUl.childNodes[0]);
-        if(e.key === "3"){
+        let targetUl = document.getElementsByTagName("ul")[indexUl]; //get destination UL
+        targetUl.insertBefore(task, targetUl.childNodes[0]); //insert active task to destination UL
+        if(e.key === "3"){ //if task moved to DONE show the welldone gif
             document.getElementById("wellDone").style.display = "block";
             setTimeout(function() {document.getElementById("wellDone").style.display = "none";}, 4000);
         }
-        tasks[targetUl.parentNode.dataset.action].unshift(task.innerText);
-        tasks[prevUl].splice(prevUlindex, 1);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        tasks[targetUl.parentNode.dataset.action].unshift(task.innerText); //push task first on target array
+        tasks[prevUl].splice(prevTaskindex, 1); //remove task from previous array
+        localStorage.setItem("tasks", JSON.stringify(tasks)); //save to local storage
 
     }
 }
+
+//on mouse out - task in no longer active and keydown event removed
 function mouseOutElement(event) {
     if(event.target.tagName !== "LI") return;
     event.target.classList.remove("active");
     document.removeEventListener("keydown", keyPressMove);
 }
     
-   
-    
-
+     
 function search (key) {
-    let liArr = document.querySelectorAll("li");
+    let liArr = document.querySelectorAll("li"); //get all li elements
     let query;
         query = document.getElementById("search").value.toUpperCase();
     for(let i = 0; i < liArr.length; i++){
         if(liArr[i].innerText.toLocaleUpperCase().includes(query))
-            liArr[i].hidden = false;
+            liArr[i].hidden = false; //show task if it include search text
         else
-            liArr[i].hidden = true;
+            liArr[i].hidden = true; //hide task if it doesn't
         
     }
 }
+
+//save tasks to API
 async function saveApi() {
-    displayLoading();
+    displayLoading(); //show loading gif
     let tempTasks = JSON.parse(localStorage.getItem("tasks"));
     const response = await fetch("https://json-bins.herokuapp.com/bin/614b1c664021ac0e6c080cef", { 
         method: "PUT",
@@ -109,10 +116,12 @@ async function saveApi() {
     if(!response.ok){
         alert("oh no! something went wrong");
     }
-    document.getElementById("loading").remove();
+    document.getElementById("loading").remove(); //remove loading gif
 }
+
+//load tasks from APi
 async function loadApi() {
-    displayLoading();
+    displayLoading(); //show loading gif
     let liArr = document.querySelectorAll("li");
     for(let i = 0; i < liArr.length; i++){
         liArr[i].remove();
@@ -125,15 +134,17 @@ async function loadApi() {
         }});
     if(!response.ok){
         alert("oh no! something went wrong");
-        document.getElementById("loading").remove();
+        document.getElementById("loading").remove(); //remove loading gif
         return;
     }
     let tasksI = await response.json();
     tasks = tasksI.tasks;
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    document.getElementById("loading").remove();
-    loadTasks();
+    localStorage.setItem("tasks", JSON.stringify(tasks)); //save to local storage
+    document.getElementById("loading").remove(); //remove loading gif
+    loadTasks(); //display tasks
 }
+
+//display loading gif
 function displayLoading() {
     let load = document.createElement("div");
     load.setAttribute("id", "loading");
@@ -142,6 +153,7 @@ function displayLoading() {
     load.classList.add("display");
 }
 
+//empty tasks object
 let tasks = {
     "todo": [],
     "in-progress": [],
